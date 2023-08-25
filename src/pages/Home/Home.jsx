ad46e01d0b0edb81919844732447bb90ada3c1e5
John Doe <johndoe@email.com>
2023-08-25 08:41:15 +0200

    Fredag morgen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -56,10 +56,34 @@ export default function Home() {
 
         // Diff in mili secs
         const diff = todayDate.getTime() - createdAtDate.getTime()
-
         const diffInDays = diff / (1000 * 3600 * 24)
 
         return Math.round(diffInDays)
+    }
+
+    const sanitizeContent = (text) => {
+        // const text = "Vi kæmper for en mere naturlig fødevareproduktion og en bæredygtig madkultur med fokus på kvalitet, omtanke og respekt for dyr og natur. Det er vores mål, at hele Danmarks fødevareproduktion bliver omlagt til enten friland eller økologi og at danskernes forbrug af kød-, æg- og mejeriprodukter i DK falder.\n\nDansk fødevareproduktion  er drevet af et ensidigt fokus på vækst, økonomi og lave omkostninger. I dag er 99% af landbrugene intensiv storproduktion med store konsekvenser  for dyrene som følge. De bliver behandlet som produktionsenheder - ikke som væsener. Det betyder, at millioner af dyr i Danmark lever under stærkt kritisable forhold."
+        const replacedText = text.replace(/\n/g, '<br>')
+        return <p dangerouslySetInnerHTML={{__html: replacedText}} />
+    }
+
+    const newsletterHandler = (e) => {
+        e.preventDefault()
+
+        const tempBody = {
+            "name": e.target.name.value,
+            "email": e.target.email.value
+        }
+
+        fetch("http://localhost:4000/api/v1/subscribers", {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify(tempBody)
+            })
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
     }
 
 
@@ -76,12 +100,13 @@ export default function Home() {
         return (
             <article className='home'>
                 {/* TOP IMAGE */}
-                <section className="home__topBanner" style={{backgroundImage: `url(${dataBanners[0].asset.url}`}}>
-                    <div className="topBanner__wrapper">
+                <section className="home__banner" style={{backgroundImage: `url(${dataBanners[0].asset.url}`}}>
+                    <div className="banner__wrapper">
                         <h1>{dataBanners[0].title}</h1>
                         <p>{dataBanners[0].content}</p>
                     </div>
                 </section>
+
     
                 {/* ABOUT */}
                 <section className="home__about">
@@ -89,68 +114,30 @@ export default function Home() {
                         return (
                             <div key={section.id} className="about__card">
                                 <h2>{section.title}</h2>
-                                <p>{section.content}</p>
+                                {sanitizeContent(section.content)}
                             </div>
                         )
                     })}
                 </section>
+
     
                 {/* FRIVILLIG */}
                 <section className="home__frivillig">
-                    <h1>Bliv Frivillig</h1>
-    
-                    {datavolunteers.map((section) => {
-                        return (
-                            <div key={section.id} className="frivillig__card">
-                                <div className="cardHeader">
-                                    <h2>{section.title}</h2>
-                                </div>
-                                <div className="cardBody">
-                                    <p>{section.content}</p>
-                                </div>
-                                <div className="cardFooter">
-                                    <p>{section?.extra}</p>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </section>
-
-                <section className="home__midBanner" style={{backgroundImage: `url(${dataBanners[1].asset.url})`}}>
-                    <h1>{dataBanners[1].title}</h1>
-                    <p>{dataBanners[1].content}</p>
-                </section>
-
-                <section className="home__nyhedsbrev">
-                    <h2>Tilmeld vores nyhedsbrev</h2>
-                    <p>Få inspiration og nyheder om dyrevelfærd og vores arbejde, direkte i din indbakke.</p>
-                    <form>
-                        <input type="text" name="email" id="email" />
-                        <input type="text" name="name" id="name" />
-                        <button type="submit">Tilmeld</button>
-                    </form>
-                </section>
-
-                <section className="home__adopter" style={{backgroundImage: `url(${dataBanners[2].asset.url})`}}>
-                    <h1>{dataBanners[2].title}</h1>
-                    <p>{dataBanners[2].content}</p>
-                </section>
-
-                <section className="home__dyr">
-                    <h2>Dyr hos os</h2>
-                    <p>{dataAnimals.length} dyr</p>
-
-                    <div className="dyr__wrapper">
-                        {dataAnimals.map((card) => {
+                    <h2>Bliv Frivillig</h2>
+                    
+                    <div className="frivillig__wrapper">
+                        {datavolunteers.map((section) => {
                             return (
-                                <div key={card.id} className="dyrCard">
-                                    <div className="dyrCard__img">
-                                        <img src={card.asset.url} alt={card.name} />
+                                <div key={section.id} className="frivillig__card">
+                                    <div className="cardHeader">
+                                        <h3>{section.title}</h3>
                                     </div>
-                                    <div className="dyrCard__text">
-                                        <h3>{card.name}</h3>
-                                        <p>{card.description}</p>
-                                        <p>Været på internat i {dateConverter(card.createdAt)} dage.</p>
+                                    <div className="cardBody">
+                                        <img src={section.asset.url} alt="" />
+                                        <p>{section.content}</p>
+                                    </div>
+                                    <div className="cardFooter">
+                                        <p>{section?.extra}</p>
                                     </div>
                                 </div>
                             )
@@ -158,6 +145,64 @@ export default function Home() {
                     </div>
                 </section>
 
+
+                {/* MID BANNER */}
+                <section className="home__banner" style={{backgroundImage: `url(${dataBanners[1].asset.url})`}}>
+                    <div className="banner__wrapper">
+                        <h1>{dataBanners[1].title}</h1>
+                        <p>{dataBanners[1].content}</p>
+                    </div>
+                </section>
+
+                {/* NYHEDSBREV */}
+                <section className="home__nyhedsbrev">
+                    <div className="nyhedsbrev__wrapper">
+                        <div className="wrapper__top">
+                            <h2>Tilmeld vores nyhedsbrev</h2>
+                            <p>Få inspiration og nyheder om dyrevelfærd og vores arbejde, direkte i din indbakke.</p>
+                        </div>
+                        <form onSubmit={newsletterHandler}>
+                            <div className="form__inputs">
+                                <input type="text" name="email" id="email" placeholder='Email' />
+                                <input type="text" name="name" id="name" placeholder='Navn' />
+                            </div>
+                            <button type="submit">Tilmeld</button>
+                        </form>
+                    </div>
+                </section>
+
+                {/* BUTTOM BANNER */}
+                <section className="home__banner" style={{backgroundImage: `url(${dataBanners[2].asset.url})`}}>
+                    <div className="banner__wrapper">
+                        <h1>{dataBanners[2].title}</h1>
+                        <p>{dataBanners[2].content}</p>
+                    </div>
+                </section>
+
+                {/* LISTE AF DYR */}
+                <section className="home__dyr">
+                    <h2>Dyr hos os</h2>
+                    <p>{dataAnimals.length} dyr</p>
+
+                    <div className="dyr__wrapper">
+                        {dataAnimals.map((card) => {
+                            return (
+                                <Link key={card.id} className="dyrCard">
+                                    <div className="dyrCard__img" style={{backgroundImage: `url(${card.asset.url})`}}>
+                                        
+                                    </div>
+                                    <div className="dyrCard__text">
+                                        <h3>{card.name}</h3>
+                                        <p>{card.description}</p>
+                                        <p className='text__dage'>Været på internat i {dateConverter(card.createdAt)} dage.</p>
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                    </div>
+                </section>
+
+                {/* FOOTER */}
                 <footer>
                     <div className="info">
                         <div className="info__left">
@@ -179,6 +224,7 @@ export default function Home() {
                         <p>&copy; 2020 - Forening for Dyrevelfærd</p>
                     </div>
                 </footer>
+
             </article>
         )
     }
